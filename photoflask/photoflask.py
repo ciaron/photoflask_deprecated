cbdec616876c098dabc56796bf642c9b743295ec
Ciaron Linstead <ciaron.linstead@gmail.com>
2017-03-02 17:54:12 +0100

    working first version, galleries and images

--- photoflask/photoflask.py
+++ photoflask/photoflask.py
@@ -7,21 +7,46 @@ app.config.from_object(__name__) # load config from this file , photoflask.py
 
 #app.config.from_envvar('PHOTOFLASK_SETTINGS', silent=True)
 
+allowed_types=['jpg', 'png', 'jpeg']
+
 @app.route('/')
 def index():
-    # get directories at top level
-    root, dirs, files = os.walk(app.static_folder)
-    #return os.walk(app.static_folder).next()
-    return app.static_folder + "<br/>Dirs: " + " ".join(dirs) + "<br/>Files: " + " ".join(files)
 
-"""
-# Print directory tree nicely
-def list_files(startpath):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('{}{}'.format(subindent, f))
-"""
+    galleries = {}
+
+    # get directories at top level
+    root, dirs, files = os.walk(app.static_folder).next()
+
+    for gallery in dirs: # each top-level dir is a gallery
+        r,d,filelist = os.walk(os.path.join(root, gallery)).next()
+
+        galleries[gallery] = {}
+
+        # remove anything from filelist that isn't an image
+        # if it's the title or description file, add those to the dict
+
+        title = gallery
+        desc = ''
+
+        if 'title.txt' in filelist:
+            with open(os.path.join(root, gallery, 'title.txt'), 'r') as t:
+                for line in t:
+                    title = line
+        if 'description.txt' in filelist:
+            pass
+
+        for f in filelist:
+            ext = os.path.basename(f).split('.')[-1]
+            if ext.lower() not in allowed_types: # i.e. not an image
+                filelist.remove(f)
+
+        images = {}
+        for f in filelist:
+            images[f] = {'title':'', 'description':'', 'path':url_for('static', filename=os.path.join(gallery,f))}
+
+            
+        galleries[gallery]={'title':title, 'description':desc, 'images':images}
+
+    #return render_template('show_galleries.html', galleries=galleries)
+    return str(galleries)
+
---
+++ setup.sh
@@ -0,0 +1,4 @@
+source activate photo
+export FLASK_APP=photoflask
+export FLASK_DEBUG=true
+echo "Now do 'flask run'"
