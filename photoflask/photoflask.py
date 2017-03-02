b50ae895d56db1c5322a3c5e4b7777f8c2a96256
Ciaron Linstead <ciaron.linstead@gmail.com>
2017-03-02 18:38:39 +0100

    read title and desc from image.txt

--- photoflask/photoflask.py
+++ photoflask/photoflask.py
@@ -9,6 +9,12 @@ app.config.from_object(__name__) # load config from this file , photoflask.py
 
 allowed_types=['jpg', 'png', 'jpeg']
 
+def get_image_info(filename):
+    with open(filename, 'r') as f:
+        data = f.readlines()
+    del(data[1]) # delete the '--' line
+    return data # a list of lines from the file.
+
 @app.route('/')
 def index():
 
@@ -35,18 +41,28 @@ def index():
         if 'description.txt' in filelist:
             pass
 
-        for f in filelist:
-            ext = os.path.basename(f).split('.')[-1]
-            if ext.lower() not in allowed_types: # i.e. not an image
-                filelist.remove(f)
+        # remove non-image files from the list
+        #for f in filelist:
+        #    ext = os.path.basename(f).split('.')[-1]
+        #    if ext.lower() not in allowed_types: # i.e. not an image
+        #        filelist.remove(f)
 
         images = {}
         for f in filelist:
-            images[f] = {'title':'', 'description':'', 'path':url_for('static', filename=os.path.join(gallery,f))}
+            ext = os.path.basename(f).split('.')[-1]
+            if ext.lower() in allowed_types: # i.e. not an image
+
+                info = os.path.join(root, gallery, os.path.basename(f).split('.')[0]+'.txt')
+                if os.path.exists(info):
+                    imgdata=get_image_info(info)
+                else:
+                    imgdata=["notitle","nodesc"]
+
+                images[f] = {'title':imgdata[0], 'description':imgdata[1:], 'path':url_for('static', filename=os.path.join(gallery,f))}
 
             
         galleries[gallery]={'title':title, 'description':desc, 'images':images}
 
-    #return render_template('show_galleries.html', galleries=galleries)
-    return str(galleries)
+    return render_template('show_galleries.html', galleries=galleries)
+    #return str(galleries)
 
